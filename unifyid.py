87b173b5b2bc47494dc0aa66b31e7f61e8149d3c
ozblod <cem.ozen@utexas.edu>
2018-02-18 20:50:38 +0300

    add generate single or multiple random methods

--- unifyid.py
+++ unifyid.py
@@ -1,23 +1,44 @@
 import requests
+from PIL import Image
+
 
 def main():
-    print(generate_random(1, 10000))
+    print(single_random_integer(1, 10000))
 
 
-def generate_random(minimum, maximum):
-    resp = requests.request('GET', "https://www.random.org/integers/",
-                            params={'num': 1,
+def generate_png():
+    img = Image.new('RGB', (128, 128))
+    img.putdata()
+
+
+def multiple_random_integers(minimum, maximum, count):
+    resp = send_random_request(minimum, maximum, count)
+
+    if resp.status_code != 200:
+        return None
+
+    lines = resp.text.splitlines()
+    return [int(line) for line in lines]
+
+
+def single_random_integer(minimum, maximum):
+    resp = send_random_request(minimum, maximum, 1)
+
+    if resp.status_code != 200:
+        return None
+
+    return int(resp.text)
+
+
+def send_random_request(minimum, maximum, count):
+    return requests.request('GET', "https://www.random.org/integers/",
+                            params={'num': count,
                                     'min': minimum,
                                     'max': maximum,
                                     'col': 1,
                                     'base': 10,
                                     'format': 'plain',
                                     'rnd': 'new'})
-
-    if resp.status_code != 200:
-        return None
-
-    return int(resp.text)
 
 
 if __name__ == "__main__":
